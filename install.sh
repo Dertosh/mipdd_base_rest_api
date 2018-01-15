73b0b7624914b9cd134ed2a7931b13835c2ad58b
Александр Миядин (Dertosh) <dertosh7@gmail.com>
2018-01-15 18:48:47 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-myproject = "mipdd_base_rest_api"
+myproject="mipdd_base_rest_api"
 virtualenv /opt/$myproject_env
 source /opt/$myproject_env/bin/activate
 pip3 install -r $myproject/req.txt
-cpus = $(cat /proc/cpuinfo | grep processor | wc -l)
+cpus=$(cat /proc/cpuinfo | grep processor | wc -l)
 echo "workers = $cpus" > $myproject/gunicorn.conf.py
 #mkdir /opt/$myproject/$myproject
 cp mipdd_api /opt/$myproject_env
