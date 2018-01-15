5b1138b6ba47f72ba5fbdfef90dbc0cc889d50f4
Александр Миядин (Dertosh) <dertosh7@gmail.com>
2018-01-15 19:18:06 +0300

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ pip3 install -r $myproject/req.txt
 cpus=$(cat /proc/cpuinfo | grep processor | wc -l)
 echo "workers = $cpus" > $myproject/gunicorn.conf.py
 #mkdir /opt/$myproject/$myproject
-cp mipdd_api /opt/$myproject_env
-cp mipdd_base_rest_api.conf /etc/supervisor/conf.d/
+cp $myproject /opt/$myproject_env
+cp $myproject.conf /etc/supervisor/conf.d/
 sudo update-rc.d supervisor enable
 sudo service supervisor start
